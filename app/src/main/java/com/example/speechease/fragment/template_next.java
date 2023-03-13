7d537041226d91f3c4394bf510f8c40f95304a0f
Navjot Makkar <125109067+navjot-makkar@users.noreply.github.com>
2023-03-13 17:11:37 -0400

    CP8117P12-231: Adding more pre-defined template in the category
    JIRA link: https://pmc.cs.uwindsor.ca/jira/browse/CP8117P12-231
    Updated the xml file to include the add button and the list components to be dynamic based on data present in database.
    Updated the template.java file to get the data of categories from database and the add operation.

---
+++ app/src/main/java/com/example/speechease/fragment/template_next.java
@@ -0,0 +1,124 @@
+package com.example.speechease.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.speechease.R;
+import com.example.speechease.add_cat;
+import com.example.speechease.add_temp;
+import com.example.speechease.modelVH.internal_vh;
+import com.example.speechease.modelVH.internall_md;
+import com.example.speechease.modelVH.temp_vh;
+import com.example.speechease.modelVH.template_md;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Locale;
+
+public class template_next extends AppCompatActivity {
+    BucketRecyclerView recyclerView;
+    TextToSpeech text;
+    private FirebaseRecyclerOptions<template_md> options;
+    private DatabaseReference databaseReference;
+    private FirebaseRecyclerAdapter<template_md, temp_vh> adapter;
+    FirebaseAuth firebaseAuth;
+
+    FloatingActionButton fab;
+    Intent i;
+    String key;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_template_cate);
+        fab = findViewById(R.id.add_fab1);
+        recyclerView = findViewById(R.id.rv_internall);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        firebaseAuth= FirebaseAuth.getInstance();
+
+
+         i = getIntent();
+        key = i.getStringExtra("key");
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Template")
+                ;
+        databaseReference.keepSynced(true);
+        Query query = databaseReference.orderByChild("key").equalTo(key);
+        options = new FirebaseRecyclerOptions.Builder<template_md>().setQuery(query,template_md.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<template_md, temp_vh>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final temp_vh tv, int i, @NonNull final template_md md) {
+
+        tv.name.setText(md.getName());
+        tv.cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int i) {
+
+                            if(i!=TextToSpeech.ERROR){
+                                // To Choose language of speech
+                                text.setLanguage(Locale.UK);
+
+                                text.speak(md.getName(),TextToSpeech.QUEUE_FLUSH,null);
+                            }
+                        }
+                    });
+            }
+        });
+
+
+
+
+
+            }
+
+
+            @NonNull
+            @Override
+            public temp_vh onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new temp_vh( LayoutInflater.from(template_next.this).inflate(R.layout.fragment_template,parent,false));
+            }
+        };
+        recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), add_temp.class);
+                intent.putExtra("key", key);
+
+                startActivity( intent );
+            }
+        });
+
+
+    }
+    protected void onStart(){
+        super.onStart();
+        adapter.startListening();
+    }
+
+
+}
