84364ff50ff2ea3eb0ed45ba410790b0332710d5
Navjot Makkar <125109067+navjot-makkar@users.noreply.github.com>
2023-03-13 17:17:34 -0400

    CP8117P12-231: Adding more pre-defined template in the category
    JIRA link: https://pmc.cs.uwindsor.ca/jira/browse/CP8117P12-231 
    Updated the xml file to include the add button and the list components to be dynamic based on data present in database. 
    Updated the template.java file to get the data of categories from database and the add operation.

---
+++ app/src/main/java/com/example/speechease/add_temp.java
@@ -0,0 +1,61 @@
+package com.example.speechease;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class add_temp extends AppCompatActivity {
+    //created two view holder and model to store and get the database from the firebase
+
+    TextView add_temp;
+    EditText name;
+    Button add;
+    Intent i;
+    String key;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cat);
+        add_temp = findViewById(R.id.add_temp);
+        name = findViewById(R.id.upcate);
+        add = findViewById(R.id.addbtn);
+        add_temp.setText("Add Template");
+        name.setHint("Template Name");
+        i = getIntent();
+        key = i.getStringExtra("key");
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!name.getText().toString().isEmpty()){
+                String key2 = FirebaseDatabase.getInstance().getReference().child("Template").push().getKey();
+                    assert key != null;
+                    DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("Template").child(key2);
+                    db.keepSynced(true);
+                    db.child("name").setValue(name.getText().toString());
+                    db.child("uid").setValue(FirebaseAuth.getInstance().getUid());
+                    db.child("key2").setValue(key2);
+                    db.child("key").setValue(key);
+                    finish();
+
+
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Name is Empty",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
